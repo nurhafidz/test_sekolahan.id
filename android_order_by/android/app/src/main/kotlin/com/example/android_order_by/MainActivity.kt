826e9cb702de3379d366ea04b1f891214195f88f
Nurhafidz Muhammad faizal <nurhafidzfaizal03@gmail.com>
2020-07-15 10:40:03 +0700

    update layout

---
+++ android_order_by/android/app/src/main/kotlin/com/example/android_order_by/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.android_order_by
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -6,15 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <style>
+        
+    </style>
 </head>
 <body>
     <div class="container mt-5">
         <h1 class="text-center">Daftar Nilai</h1>
-        <table class="table table-striped">
+        <table class="table text-white ">
             <tr>
-                <td><h2>Nama</h2></td>
-                <td><h2>Nilai</h2></td>
-                <td><h2>Rangking</h2></td>
+                <td>Nama</td>
+                <td>Nilai</td>
+                <td>Rangking</td>
             </tr>
             @php
                 $i=1;
